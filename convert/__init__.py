e8a942d7d2263015c5570882dcdcee839f5a6aad
AkmalHstl <akmalrbc6@gmail.com>
2024-10-01 23:01:08 +0800

    perubahan struktur

---
+++ convert/__init__.py
@@ -0,0 +1,9 @@
+from .package_berat.fungsi_berat import berat
+
+
+
+
+# from .Package_kecepatan.fungsi_kecepatan import 
+# from .package_berat.fungsi_suhu import
+# from .package_berat.fungsi_panjang import
+# from .package_berat.fungsi_waktu import
