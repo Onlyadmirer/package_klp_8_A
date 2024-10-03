5e1d3f4146da258a111aa605a7064f0f096fd046
MONTANA <yudbryawan@BRYAWANMONTANA.local>
2024-10-03 13:39:05 +0800

    fix: fixing project structure

--- fraier/__init__.py
+++ fraier/__init__.py
@@ -1,6 +1,5 @@
-from .package_berat import fungsi_berat
-
-from .package_berat.fungsi_berat import berat
+from .fungsi_berat import berat
+from .fungsi_kecepatan import kecepatan
 
 # from .Package_kecepatan.fungsi_kecepatan import 
 # from .package_berat.fungsi_suhu import
