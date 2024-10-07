f3845e4b222a5dc8cbcd1fddad018a2f3643cf33
Assyiekril <arilsarda21@gmail.com>
2024-10-06 17:37:57 -0700

    Update fungsi suhu

--- fraier/fungsi_suhu.py
+++ fraier/fungsi_suhu.py
@@ -1,27 +1,28 @@
 def suhu(satuan1, satuan2, nilai):
 
     if satuan1 == 'c' and satuan2 == 'f':
-        return f"{(nilai * 9/5) + 32:.2f}"
+        return (nilai * 9/5) + 32
     elif satuan1 == 'c' and satuan2 == 'k':
-        return f"{(nilai + 273.15):.2f}"
+        return (nilai + 273.15)
     elif satuan1 == 'c' and satuan2 == 'r':
-        return f"{(nilai * 4/5):.2f}"
+        return (nilai * 4/5)
     elif satuan1 == 'f' and satuan2 == 'c':
-        return f"{((nilai * 5/9) + 32):.2f}"
+        return ((nilai * 5/9) + 32)
     elif satuan1 == 'f' and satuan2 == 'k':
-        return f"{(nilai - 32) * 5/9 + 273.15}"
+        return (nilai - 32) * 5/9 + 273.15
     elif satuan1 == 'f' and satuan2 == 'r':
-        return f"{(nilai * 4/9 - 32):.2f}"
+        return (nilai * 4/9 - 32)
     elif satuan1 == 'k' and satuan2 == 'c':
         return (nilai - 273.15)
     elif satuan1 == 'k' and satuan2 == 'f':
-        return f"{((nilai - 273.15) * 9/5 + 32):.2f}"
+        return ((nilai - 273.15) * 9/5 + 32)
     elif satuan1 == 'k' and satuan2 == 'r':
-        return f"{(nilai * 4/5 - 273.15):.2f}"
+        return (nilai * 4/5 - 273.15)
     elif satuan1 == 'r' and satuan2 == 'c':
-        return f"{(nilai * 5/4):.2f}"
+        return (nilai * 5/4)
     elif satuan1 == 'r' and satuan2 == 'f':
-        return f"{(nilai * 9/4 + 32):.2f}"
+        return (nilai * 9/4 + 32)
     elif satuan1 == 'r' and satuan2 == 'k':
-        return f"{(nilai * 5/4 + 273.15):.2f}"
+        return (nilai * 5/4 + 273.15)
     
+    return round(nilai, 2)
